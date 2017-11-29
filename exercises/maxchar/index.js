d576c1b19c844e9f5f8919f5ce9680284423d890
theperfectfuel <robert@theperfectfuel.com>
2017-11-29 01:25:03 -0800

    added maxchar solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (let x of str) {
+        if (!charMap[x]) {
+            charMap[x] = 1;
+        } else {
+            charMap[x]++;
+        }
+    };
+
+    for (let y in charMap) {
+        if (charMap[y] > max) {
+            max = charMap[y];
+            maxChar = y;
+        }
+    };
+    return maxChar;
+}
 
 module.exports = maxChar;
